c569856347ee0904a35482d70cb8ed6424aa8481
Desarrollo2 <desarrollo2@ati.net.co>
2026-03-25 14:55:59 -0500

    Initial commit

---
+++ TechnicalTest.ClassLibrary/Interfaces/IEntity.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace TechnicalTest.ClassLibrary.Interfaces
+{
+    public class IEntity
+    {
+        [Display(Name = "Habilitado")]
+        public bool Status { get; set; } = true;
+
+        [Display(Name = "Creado")]
+        public DateTime Created { get; set; } = DateTime.Now;
+
+        [Display(Name = "Modificado")]
+        public DateTime Updated { get; set; } = default(DateTime);
+    }
+
+}
---
+++ TechnicalTest.ClassLibrary/Interfaces/IEntityDelete.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace TechnicalTest.ClassLibrary.Interfaces
+{
+    public class IEntityDelete : IEntity
+    {
+        [Display(Name = "Eliminado")]
+        public DateTime? Deleted { get; set; } = null;
+    }
+}
